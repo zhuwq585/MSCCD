b3658072109d1941c9e85a5361c1d23ca424be38
SnO2WMaN <me@sno2wman.net>
2022-12-13 18:59:48 +0000

    devcontainer misc

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,3 @@
+mvn install:install-file -Dfile=./lib/antlr-4.8-modified.jar -DgroupId=org.nagoya_u.ertl.sa -DartifactId=antlr-v4.8-modified -Dversion=4.8 -Dpackaging=jar
+
+pip install jinja2 ujson
