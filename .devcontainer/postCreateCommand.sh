e4dffb614f1d94d91741a1aa59e01da7c62ce4a6
SnO2WMaN <me@sno2wman.net>
2022-12-30 01:02:32 +0900

    Update .devcontainer and README-ja

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,3 +1,8 @@
-mvn install:install-file -Dfile=./lib/antlr-4.8-modified.jar -DgroupId=org.nagoya_u.ertl.sa -DartifactId=antlr-v4.8-modified -Dversion=4.8 -Dpackaging=jar
+mvn install:install-file \
+  -Dfile=./lib/antlr-4.8-modified.jar \
+  -DgroupId=org.nagoya_u.ertl.sa \
+  -DartifactId=antlr-v4.8-modified \
+  -Dversion=4.8 \
+  -Dpackaging=jar
 
 pip install jinja2 ujson
---
+++ README-ja.md
@@ -0,0 +1,7 @@
+# MSCCD
+
+## Developing
+
+### Setup with devcontainer
+
+devcontainerを使って環境を構築することが出来ます．
